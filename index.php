d7284aca228eb2edbbf53654d43ed74af2d076a6
Daniel Knoll <knollmdan@gmail.com>
2023-01-05 16:45:08 -0500

    added form to home page

--- index.php
+++ index.php
@@ -10,5 +10,11 @@
 </head>
 <body>
     <h1>Pick a colour</h1>
+    <form action="confirm.php" method="POST">
+        <label>Red<input type="radio" value="red" name="radio1"></label><br>
+        <label>Blue<input type="radio" value="blue" name="radio1"></label><br>
+        <label>Yellow<input type="radio" value="yellow" name="radio1"></label><br>
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
